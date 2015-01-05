8f0aa1f0b4e6e1f4e5ae5f25a0c609aa70d6fef1
gailees <gailees@umich.edu>
2015-01-05 07:28:59 -0500

    messing around with autolayout

---
+++ StrutsProblem/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  StrutsProblem
+//
+//  Created by David Fontenot on 1/5/15.
+//  Copyright (c) 2015 HackMatch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var topLeftView: UIView!
+    @IBOutlet var topRightView: UIView!
+    @IBOutlet var bottomView: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
